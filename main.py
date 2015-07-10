ccbabcc7477a2a6555fabc395a2414c1ac5568c1
Taiki Nakaoka <nakaokataiki@gmail.com>
2015-07-11 04:13:01 +0900

    my first commit

---
+++ main.py
@@ -0,0 +1,189 @@
+# -*- coding: utf-8 -*-
+# example
+alice = "For a minute or two she stood looking at the house, and wondering what to do next, when suddenly a footman in livery came running out of the wood--(she considered him to be a footman because he was in livery: otherwise, judging by his face only, she would have called him a fish)--and rapped loudly at the door with his knuckles. It was opened by another footman in livery, with a round face, and large eyes like a frog; and both footmen, Alice noticed, had powdered hair that curled all over their heads. She felt very curious to know what it was all about, and crept a little way out of the wood to listen."
+
+bbc_greek = u"""
+The Eurogroup has described new Greek proposals on securing a vital third bailout as "thorough".
+President Jeroen Dijsselbloem said a "major decision... whichever way" could now be made at a eurozone finance meeting on Saturday.
+The plans are aimed at staving off a possible Greek exit from the eurozone.
+Greek PM Alexis Tsipras will put the plans, which contains many elements rejected in a referendum last Sunday, to a vote in parliament on Friday.
+The prime minister submitted the proposals to Greece's creditors - the European Commission, the European Central Bank and the International Monetary Fund - by the Thursday deadline they had set.
+Follow the latest updates here
+European Commission President Jean-Claude Juncker, European Central Bank President Mario Draghi, International Monetary Fund head Christine Lagarde and Mr Dijsselbloem were scheduled to hold a conference call on the new proposals at 11:00 GMT.
+Mr Dijsselbloem said the new Greek paper was "a thorough piece of text" and that support from the Greek parliament would give it "more credibility".
+"But even then we need to consider carefully whether the proposal is good and if the numbers add up. We have to make a major decision. Whichever way."
+null
+Analysis: Robert Peston, BBC economics editor
+Only a few days ago Mr Tsipras won an overwhelming mandate from the Greek people, in a referendum, to reject more-or-less these bailout terms.
+And today, on the back of that popular vote, he is signing up to the supposedly hated bailout. This is big politics that would make Lewis Carroll proud.
+But here's the point. If a way isn't found to allow the banks to reopen within days - and the ECB simply maintaining Emergency Liquidity Assistance won't come anywhere near to achieving that - the Greek economy will implode so that any bailout deal agreed this weekend will become irrelevant in weeks.
+Read more from Robert
+Crisis triggers memories of World War Two
+Angela Merkel under pressure over Greek bailout deal
+All Greek to you? Debt jargon explained
+null
+Eurozone finance ministers meet in Brussels on Saturday. A meeting of Eurogroup leaders is scheduled for Sunday afternoon, followed by a full EU summit two hours later.
+Italian Prime Minister Matteo Renzi said he was optimistic and hoped a deal could be struck on Saturday so that the Sunday meetings would not be needed.
+French President Francois Hollande said the new proposals were "serious and credible" and that the "Greeks have just shown their determination to remain in the eurozone".
+Greece in numbers
+€320bn
+Greece's debt mountain
+€240bn
+European bailout
+177% country's debt-to-GDP ratio
+25% fall in GDP since 2010
+26% Greek unemployment rate
+Source: ECB, IMF, Greek National Statistics Agency
+Reuters
+However Germany, which has been far less upbeat on a deal, would not comment in detail on the new plan.
+Government spokesman Steffen Seibert said it was withholding judgment and waiting for the creditor institutions to give their opinion.
+Finance ministry spokesman Martin Jaeger said Germany would not accept any reduction of debt that caused it more losses.
+"The outcome of the Eurogroup meeting on Saturday is completely open," he said.
+'In this together'
+In Athens, Mr Tsipras convened a meeting of his Syriza party ahead of the Greek parliamentary debate.
+A government official quoted him as telling the party's lawmakers that the referendum had given him a mandate to seek a better deal but not to leave the eurozone.
+"We are all in this together," he was quoted as saying.
+Jump media playerMedia player helpOut of media player. Press enter to return or tab to continue.
+Media caption
+Greek views on tough new proposals
+Syriza parliamentary spokesman Nikos Filis said he was confident MPs would give the government the mandate to negotiate the new bailout package.
+The coalition government has 162 seats in the 300-strong parliament, and also has the backing of many opposition MPs.
+However, the BBC's Mark Lowen in Athens says that no matter how it is packaged and whatever the positive spin, Mr Tsipras has made a major climb-down, with the measures he has agreed similar to what the creditors were demanding.
+Further rallies for and against a new bailout are planned in Athens on Friday.
+Banks closed
+The measures submitted in the new Greek document include:
+tax rise on shipping companies
+unifying VAT rates at standard 23%, including restaurants and catering
+phasing out solidarity grant for pensioners by 2019
+€300m ($332m; £216m) defence spending cuts by 2016
+privatisation of ports and sell-off of remaining shares in telecoms giant OTE
+scrapping 30% tax break for wealthiest islands
+Greece's creditors have already provided more than €200bn in two bailouts since a rescue plan began five years ago.
+The second bailout expired on 30 June.
+Greece's banks are still closed and the €60 (£43; $66) daily limit on cash machine withdrawals for Greeks, imposed on 28 June, remains in force. With a shortage of €20 notes, for many the withdrawal is in effect €50.
+null
+Crisis countdown
+10 July: Greek parliament vote. ECB, EU and IMF discuss proposals at technical level
+11 July: Eurozone finance ministers discuss plans (Brussels 13:00 GMT)
+12 July: Eurogroup leaders meet (14:00 GMT) followed by summit of all 28 members of the European Union (16:00 GMT). Both Brussels
+20 July: €3bn payment due from Greece to the European Central Bank
+Tsipras, Greece's high-stakes gambler
+Why Greece sees France as last hope
+How easy is it to swap currencies?
+Full coverage of Greek debt crisis
+null
+Greek scenarios
+"""
+
+news = u"""
+Standing on the Washington Mall at the turn of the new millennium, it was impossible not to be struck by America's power and global pre-eminence.
+Victory in the Cold War made it the hegemon in a unipolar world.
+Few argued when the 20th Century was dubbed the "American Century", a term first coined in the early 1940s when the country was still overcoming its isolationist instincts.
+Even the New Year's fireworks, which illuminated the obelisk of the Washington Monument in a way that made it resemble a giant number one, projected the country's supremacy as the world's sole superpower.
+Over the past 15 years, America's fortunes have changed with dizzying speed.
+First came the tremors: the dot-com bust and a disputed presidential election in 2000. Then came the massive convulsions: the destruction of the Twin Towers in 2001 and the collapse of Lehman Brothers in 2008.
+Long wars in Afghanistan and Iraq have exacted an enormous blood price - the lives of 6,852 American military personnel - not to mention immense financial expense, estimated to be as high as $6 trillion (£3.9tn).
+The detention centre at Guantanamo Bay has undermined American ideals, just as the NSA and Wikileaks spying scandals have undercut American diplomacy.
+George W Bush, a president with a Manichean worldview, was widely seen as over-eager to project America's military might, without adequately considering the long-term consequences.
+Barack Obama, who campaigned in 2008 on a platform of extricating America from its unpopular and exhausting wars, has drawn criticism for disengaging too much.
+Under both presidents - the first an impulsive unilateralist, the second an instinctive multilateralist content sometimes to lead from behind - America's global standing has been diminished.
+Lost fear factor
+Polls regularly show that Americans recognise that their country's international standing has waned.
+Among the young, this trendline has fallen sharply. Only 15% of 18-29-year-olds believe that America is the "greatest country in the world", according to Pew, down from 27% in 2011.
+Tellingly, however, there has been no great public outcry.
+No longer is there much appetite for America playing its long-standing role of global policeman, even in the face of the rise of the group calling itself Islamic State.
+The cost, human and financial, is considered too great. Americans increasingly think that other countries should share the burden.
+Obama, while continuing to trumpet "American exceptionalism", regularly prefaces remarks on foreign affairs by acknowledging the limits of US power, again with little public outcry.
+America's reluctance to launch new military actions has also had a major bearing on the nuclear negotiations with Iran
+The upshot is that the United States is no longer so keen to exert leadership in an increasingly messy world.
+Yet one of the reasons why the world has become so disorderly is because America is no longer so active in imposing order.
+Over the course of this century Washington has lost its fear factor.
+Ignoring the White House
+World leaders nowadays seem prepared to provoke the wrath of the White House, confident that it will never rain down on them.
+It explains why the Syrian President Bashar al-Assad, after unleashing chemical weapons against his people, continues to bombard them with barrel bombs.
+Why Vladimir Putin annexed Crimea, and also offered a safe haven for the NSA whistleblower Edward Snowden.
+And also why Benjamin Netanyahu thumbed his nose at the Obama administration, by accepting an invitation from the Republican congressional leadership to address a joint session of Congress, a platform he used to lambast the Iran nuclear deal.
+Assad's flouting of American warnings is especially noteworthy.
+In killing so many civilians with chemical weapons, he flagrantly crossed the "red line" imposed by Obama, but escaped punishment.
+The president was unwilling to carry through on an explicit threat, in what was the biggest foreign policy climbdown of his presidency and also one of the most significant in the past 50 years.
+Even supporters of Barack Obama believe he made a fatal strategic mistake, because it demonstrated endless flexibility and a lack of American resolve.
+Needless to say, despots around the world took note.
+Weak hand
+America's reluctance to launch new military actions has also had a major bearing on the nuclear negotiations with Iran.
+Tehran has managed to extract notable concessions, such as the ongoing ability to enrich uranium, hitherto ruled out by the Americans.
+Obama recognises intellectually that he could do far more in terms of massaging the egos of world leaders, but cannot quite bring himself to do so
+It has played a weak hand strongly, because it knows that America has what the foreign affairs columnist Thomas Friedman calls "an empty holster".
+Nor is it just America's enemies who no longer fear the White House to the extent they once did.
+In recent months, two close allies, Britain and Australia, have defied the Obama administration by joining the Asian Infrastructure Investment Bank.
+By signing up to the AIIB, they are effectively endorsing Beijing's effort to establish financial rivals to the Bretton Woods institutions, the World Bank and International Monetary Fund (IMF), which are dominated by America.
+Ambiguous language
+By seeking improved commercial and diplomatic relations with China, Britain and Australia are also hedging.
+They suspect that America will not be the dominant Pacific military power indefinitely, nor the world's foremost economic powerhouse.
+Other American allies would complain that the "dependability factor" has also gone.
+Israel feels badly let down by the Obama administration over the Iran deal, and relations between Benjamin Netanyahu and Barack Obama are poisonous.
+The president, by using deliberately ambiguous language, has even signalled that his administration might end its traditional protection of Israel at the United Nations.
+Like Israel, Saudi Arabia has been enraged by the prospective nuclear deal with the Iranians.
+Riyadh also knows that America is no longer so dependent on its oil, the cornerstone of the relationship since the end of World War Two.
+Egypt was angered in 2012 when Obama said Cairo was neither an ally or an enemy.
+Later, the State Department issued an embarrassing correction, and reinstated Cairo as a "major non-Nato ally."
+No massaging
+Maybe Obama's Egyptian error, and the slight it conveyed, was truly a Freudian slip.
+After all, he hasn't invested the same energy nurturing alliances as his predecessors. The detached air that has been a hallmark of his presidency also extends to foreign affairs.
+America's diplomacy has also been complicated by the dysfunction and hyper-partisanship in Washington
+Here, I gather, Obama recognises intellectually that he could do far more in terms of massaging the egos of world leaders, but cannot quite bring himself to do so.
+Indeed, a common complaint is that the Obama administration has prioritised normalising relations with its one-time enemies, Iran and Cuba, at the expense of fostering longstanding friendships.
+Realising that America is no longer so supportive, and no longer so engaged in the Middle East, the Saudis have recently taken military action of their own in Yemen.
+There's also been a warming of relations between Riyadh and Moscow.
+And Egypt launched airstrikes in February against the Islamic State group in Libya.
+America's standing in the Middle East has unquestionably waned, along with its ability to shape events.
+Unexpected stats
+More surprising has been its slippage in Africa, Obama's ancestral home, and Asia, the focus of his much vaunted pivot.
+In Asia, America's median approval rating in 2014, as measured by Gallup, was 39%, a 6% drop since 2011.
+In Africa, the median approval went down to 59%, the lowest since polling began, despite Obama hosting the US-Africa Leaders' Summit in Washington in August, last year.
+It even dropped in Kenya, his father's birthplace.
+America's diplomacy has also been complicated by the dysfunction and hyper-partisanship in Washington.
+Republican lawmakers actively sought to derail the Iran nuclear deal by sending a letter to the supreme leader Ayatollah Ali Khamenei.
+President or Congress?
+House speaker John Boehner invited Netanyahu to address Congress, knowing it would infuriate the White House.
+Democrats with reservations about free trade have tried to sabotage the Trans-Pacific Partnership, the biggest trade deal since Nafta.
+There's also been strong congressional opposition to one of the big plays of Obama's second term, the rapprochement with Cuba.
+Should countries listen to the president or Congress?
+America cannot even lay claim any more to its great, uncontested boast since 1872, of being the world's largest economy.
+The IMF now estimates that China's economy is fractionally bigger.
+Yet it would be a mistake to exaggerate the downsizing of American influence.
+US military spending continues to dwarf its rivals, and up until last year amounted to more than the next 10 countries combined.
+In 2014, America spent $731bn, compared to China's $143bn.
+Even though China's economy is now larger, America's per capita spending power is in a different league - $53,000 to $11,868.
+Though America is contending with the rise of the rest - China, India, Brazil, Germany and Russia - it has not yet been overtaken by emergent rivals.
+Indeed, there are foreign policy thinkers here who predict that America will preserve its pre-eminence for at least another 20 years.
+Yet the unipolar moment ushered in by the fall of the Berlin Wall has proved to be just that: momentary.
+Moreover, hopes of a new world order following the collapse of the Soviet Union have given way to widespread pessimism about the spread, even the contagion, of global disorder.
+Gone are the certainties of America's Cold War thinking, when the containment of communism governed its international actions.
+Gone are the doctrines that gave US foreign policy such a rigid frame, throughout the Cold War and in the aftermath of 9/11.
+Gone, too, is the notion that every fight is an American fight and along with it a redefinition of what constitutes the US national interest.
+Barack Obama has instead advocated pragmatism and diplomatic dexterity, trying to steer a path between America being overextended and undercommitted.
+Maybe the overriding challenge for US diplomacy over the next 20 years is to strike the proper balance.
+"""
+
+ignore = "a the be is are he she they it to"
+
+def main(text, ignore):
+    """
+    text: str
+    ignore: str
+    return: dict
+    """
+    words = text.split(" ")
+    ignore_words = ignore.split(" ")
+    result = {}
+    for word in words:
+        if word not in ignore_words and word not in result:
+            result[word] = 1
+        elif word not in ignore_words and word in result:
+            result[word] += 1
+
+    return result
+
+for k, v in main(news, ignore).items():
+    if v > 1:
+        print k, v
