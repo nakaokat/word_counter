41e3a49dc3b59dc806f491ecae04ca8ad369bc6b
Taiki Nakaoka <nakaokataiki@gmail.com>
2015-07-11 17:31:31 +0900

    feat: add text input for users

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 import main
 
 app = Flask(__name__)
@@ -18,9 +18,18 @@ def words():
             result += k + " " + str(v)  + "<br>"
     return result
 
-
+@app.route('/input', methods=['GET','POST'])
+def input():
+    if request.method == 'POST':
+        text = request.form['text']
+        result = "<h3>result</h3>"
+        for k, v in main.main(text, main.ignore).items():
+            if v > 1:
+                result += k + " " + str(v)  + "<br>"
+        return render_template("input.html", output=result)
+    else:
+        return render_template("input.html")
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
