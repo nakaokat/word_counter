412066c724952f0a36b18474963dfae0e623fd6b
Taiki Nakaoka <nakaokataiki@gmail.com>
2015-07-19 20:32:40 +0900

    refactor: delete test page /words

--- run.py
+++ run.py
@@ -1,18 +1,8 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, render_template, request, redirect
-import main
-
+import main, test
 app = Flask(__name__)
 
-
-@app.route('/words')
-def words():
-    result = ""
-    threshold = 0
-    for k, v in main.main(main.news, main.ignore).items():
-        if v > threshold:
-            result += k + " " + str(v)  + "<br>"
-    return result
 
 @app.route('/', methods=['GET','POST'])
 def input():
