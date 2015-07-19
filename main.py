82210a152b30676de23f93f4fffcd55b423e2b38
Taiki Nakaoka <nakaokataiki@gmail.com>
2015-07-19 19:55:47 +0900

    refactor: not word is ignored

---
+++ main.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+ignore = "a the be is are he she they it to"
+no_ignore = ""
+
+def main(text, no_ignore):
+    """
+    text: str
+    ignore: str
+    return: dict
+    """
+    words = text.split(" ")
+    ignore_words = ignore.split(" ")
+    result = {}
+    for word in words:
+        if word not in ignore_words and word not in result:
+            result[word] = 1
+        elif word not in ignore_words and word in result:
+            result[word] += 1
+
+    return result
