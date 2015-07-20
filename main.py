ba18884718ba0ab86cc5fea52f6b4c9c049e2279
Taiki Nakaoka <nakaokataiki@gmail.com>
2015-07-21 02:37:45 +0900

    feat: now result is ordered by appearance.

--- main.py
+++ main.py
@@ -19,3 +19,19 @@ def main(text, ignore):
             result[word] += 1
 
     return result
+
+
+def order(text, ignore):
+    """
+    :param text:
+    :param ignore:
+    :return: list which is ordered
+    this is used to order a dictionary of counter.
+    """
+    words = text.split(" ")
+    ignore_words = ignore.split(" ")
+    result = []
+    for word in words:
+        if word not in ignore_words and word not in result:
+            result.append(word)
+    return result
--- run.py
+++ run.py
@@ -11,7 +11,8 @@ def input():
         text = request.form['text']
         result = main.main(text, main.no_ignore)
         threshold = int(request.form["threshold"])
-        return render_template("input.html", result=result, threshold=threshold)
+        order = main.order(text, main.no_ignore)
+        return render_template("input.html", result=result, threshold=threshold, order=order)
     else:
         return render_template("input.html", result=result)
 
